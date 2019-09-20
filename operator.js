928f90cd4d4290686cb835d3f5001289b4d81d79
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-09-20 23:43:16 +0700

    object entries

--- operator.js
+++ operator.js
@@ -7,3 +7,14 @@ copyOfGreatThings.push('foo');
 
 console.log(greatThings);
 console.log(copyOfGreatThings);
+
+
+let person = {
+    name: 'Trung Ta',
+    age: 24,
+    job: 'Dev'
+};
+
+for (let [prop, value] of Object.entries(person)) {
+    console.log(prop, value);
+}
